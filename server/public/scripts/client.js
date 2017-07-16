6627c442ab017403626f68ed0aa84e3d67da30da
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-07-16 17:50:15 -0500

    made code DRYer, added comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,8 +2,7 @@ console.log('js sourced');
 
 var myApp = angular.module('myApp', []);
 
-var photos = []
-
+//object constructor for the pictures
 function Picture(link, description){
   this.link = link,
   this.likes = 0,
@@ -14,6 +13,7 @@ function Picture(link, description){
   this.timesViewed = 0
 }
 
+//creating 6 pictures using picture constructor
 var tayDex = new Picture('taydex.jpeg', "Me and my girlfriend Taylor!");
 var family = new Picture('family.jpg', "I love my crazy family!");
 var david = new Picture('david.jpg', "David is one of my childhood best friends!");
@@ -21,29 +21,32 @@ var elSalvador = new Picture('elsalvador.jpg', "My missions trip to El Salvador 
 var graduation = new Picture('graduation.jpg', "My graduation day is one of my proudest moments!");
 var swing = new Picture('swing.jpg', "Swing dancing is one of my favorite things to do and I love all of my friends I have made there!");
 
+//create controller
 myApp.controller('GalleryController', function(){
+  //create shorthand variable for controller
   var gallery = this;
-  gallery.photos = photos;
-  gallery.photos.push(tayDex, family, david, elSalvador, graduation, swing);
+  //add pictures to an array in gallery controller
+  gallery.photos = [tayDex, family, david, elSalvador, graduation, swing];
+  //increase like count on photo when like button is clicked
   gallery.likeButton = function(photo){
     photo.likes +=1;
   };
-  gallery.toggle = function(photo){
-    console.log(photo.descVisible);
+  //toggle description of photo when the photo or description is clicked
+  gallery.toggleDesc = function(photo){
     photo.descVisible = !photo.descVisible
+    //add 1 to times viewed whenever the photo is clicked and not the description
     if(photo.descVisible){
       photo.timesViewed +=1;
     }
-    console.log(photo.descVisible);
   }
+  //add comment to array of comments on picture
   gallery.addComment = function(comment, photo){
     photo.comments.push(comment);
-    console.log(photo.comments);
   }
+  //toggle the display of comment son specific picture
   gallery.toggleComments = function(photo){
-    console.log(photo.showComments);
     photo.showComments = !photo.showComments;
-    console.log(photo.showComments);
   }
+  //log the contoller
   console.log(gallery);
 })
