9f7c77494f4cb0ee5eb234e50d724bf14aa95b4c
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-07-16 17:38:25 -0500

    finished project

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,45 +9,41 @@ function Picture(link, description){
   this.likes = 0,
   this.comments = [],
   this.description = description,
-  this.descVisible = false
+  this.descVisible = false,
+  this.showComments = false,
+  this.timesViewed = 0
 }
 
 var tayDex = new Picture('taydex.jpeg', "Me and my girlfriend Taylor!");
 var family = new Picture('family.jpg', "I love my crazy family!");
-// var tayDex = {
-//   link: 'taydex.jpeg',
-//   likes: 0,
-//   comments: [],
-//   description: "Me and my girlfriend Taylor!",
-//   descVisible: false,
-// }
-// var family = {
-//   link: 'family.jpg',
-//   likes: 0,
-//   comments: [],
-//   description: "I love my crazy family",
-//   descVisible: false,
-// }
+var david = new Picture('david.jpg', "David is one of my childhood best friends!");
+var elSalvador = new Picture('elsalvador.jpg', "My missions trip to El Salvador is one of my favorite memories!");
+var graduation = new Picture('graduation.jpg', "My graduation day is one of my proudest moments!");
+var swing = new Picture('swing.jpg', "Swing dancing is one of my favorite things to do and I love all of my friends I have made there!");
 
 myApp.controller('GalleryController', function(){
   var gallery = this;
   gallery.photos = photos;
-  gallery.photos.push(tayDex, family);
+  gallery.photos.push(tayDex, family, david, elSalvador, graduation, swing);
   gallery.likeButton = function(photo){
-    console.log('like button clicked');
-    console.log('photo', photo);
     photo.likes +=1;
-    console.log(photo.likes);
-    console.log(gallery);
   };
   gallery.toggle = function(photo){
     console.log(photo.descVisible);
     photo.descVisible = !photo.descVisible
+    if(photo.descVisible){
+      photo.timesViewed +=1;
+    }
     console.log(photo.descVisible);
   }
   gallery.addComment = function(comment, photo){
     photo.comments.push(comment);
     console.log(photo.comments);
+  }
+  gallery.toggleComments = function(photo){
+    console.log(photo.showComments);
+    photo.showComments = !photo.showComments;
+    console.log(photo.showComments);
   }
   console.log(gallery);
 })
