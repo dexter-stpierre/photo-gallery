bd06d66e319f4ccf555fa3015f4562392d076fa1
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-07-16 15:30:08 -0500

    added pictures, like button, and descriptions

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,8 +2,37 @@ console.log('js sourced');
 
 var myApp = angular.module('myApp', []);
 
+var photos = []
+var tayDex = {
+  link: 'taydex.jpeg',
+  likes: 0,
+  comments: [],
+  description: "Me and my girlfriend Taylor!",
+  photoVisible: true
+}
+var family = {
+  link: 'family.jpg',
+  likes: 0,
+  comments: [],
+  description: "I love my crazy family",
+  photoVisible: true
+}
+
 myApp.controller('GalleryController', function(){
   var gallery = this;
+  gallery.photos = photos;
+  gallery.photos.push(tayDex, family);
+  gallery.likeButton = function(photo){
+    console.log('like button clicked');
+    console.log('photo', photo);
+    photo.likes +=1;
+    console.log(photo.likes);
+    console.log(gallery);
+  };
+  gallery.toggle = function(photo){
+    console.log(photo.photoVisible);
+    photo.photoVisible = !photo.photoVisible
+    console.log(photo.photoVisible);
+  }
   console.log(gallery);
-
 })
